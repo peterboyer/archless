45e76ea17d092fc4c3c3ab309c0fa322879efb43
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2022-08-12 00:06:42 +1000

    feat: startx bspwm install and config

--- install.sh
+++ install.sh
@@ -113,7 +113,9 @@ fi
 
 yes | pacstrap /mnt \
   base base-devel linux linux-firmware $UCODE \
-  grub efibootmgr networkmanager sof-firmware
+  grub efibootmgr networkmanager sof-firmware \
+  xorg xorg-xinit \
+  bspwm sxhkd alacritty ttf-jetbrains-mono
 
 # https://wiki.archlinux.org/title/Installation_guide#Chroot
 
@@ -169,3 +171,43 @@ chpasswd <<< "root:password"
 useradd -mG wheel $oUSER
 chpasswd <<< "$oUSER:password"
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
+
+# https://wiki.archlinux.org/title/Xinit#Configuration
+
+cat << EOF > /home/$oUSER/.xinitrc
+#!/bin/sh
+
+userresources=\$HOME/.Xresources
+usermodmap=\$HOME/.Xmodmap
+sysresources=/etc/X11/xinit/.Xresources
+sysmodmap=/etc/X11/xinit/.Xmodmap
+
+if [ -f "\$sysresources" ]; then xrdb -merge "\$sysresources"; fi
+if [ -f "\$sysmodmap" ]; then xmodmap "\$sysmodmap"; fi
+if [ -f "\$userresources" ]; then xrdb -merge "\$userresources"; fi
+if [ -f "\$usermodmap" ]; then xmodmap "\$usermodmap"; fi
+
+if [ -d /etc/X11/xinit/xinitrc.d ] ; then
+ for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+  [ -x "\$f" ] && . "\$f"
+ done
+ unset f
+fi
+
+sxhkd &
+exec bspwm
+EOF
+
+# https://wiki.archlinux.org/title/Bspwm#Configuration
+
+install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc /home/$oUSER/.config/bspwm/bspwmrc
+install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc /home/$oUSER/.config/sxhkd/sxhkdrc
+sed -i 's/urxvt/alacritty/g' /home/$oUSER/.config/sxhkd/sxhkdrc
+
+# https://wiki.archlinux.org/title/Xinit#Autostart_X_at_login
+
+cat << EOF >> /home/$oUSER/.bash_profile
+if [ -z "\$DISPLAY" ] && [ "\$XDG_VTNR" -eq 1 ]; then
+  exec startx
+fi
+EOF
