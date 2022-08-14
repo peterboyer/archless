c4da2d8a2fb9d85f96d5806590ce87e67262db89
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2022-08-15 09:22:55 +1000

    fix: add sudo ln + mkconfig code

--- sudo.sh
+++ sudo.sh
@@ -1,7 +1,7 @@
 # grub
 
-sed -i "s/GRUB_TIMEOUT=.+/GRUB_TIMEOUT=0/g" /etc/default/grub
-echo "GRUB_HIDDEN_TIMEOUT=0" >> /etc/default/grub
-echo "GRUB_HIDDEN_TIMEOUT_QUIET=true" >> /etc/default/grub
-
-grub-mkconfig -o /boot/grub/grub.cfg
+(
+  cd /etc/default;
+  ln -fs /home/$oUSER/config/grub;
+  grub-mkconfig -o /boot/grub/grub.cfg
+)
